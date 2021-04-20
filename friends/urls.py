12f53c5d26f69b6832c152b5fcaba769f751cc0b
Abhishek Tiwari <abhie.lp@gmail.com>
2021-04-20 23:24:18 +0530

    Show all friend requests of logged-in user

--- friends/urls.py
+++ friends/urls.py
@@ -2,9 +2,11 @@
 
 from django.urls import path
 from .api import send_friend_request_view
+from .views import my_friend_requests_view
 
 app_name = "friends"
 
 urlpatterns = [
+    path("requests/", my_friend_requests_view, name="friend_requests"),
     path("be-my-friend/", send_friend_request_view, name="send_request"),
 ]
--- friends/views.py
+++ friends/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http.response import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
+from django.db import connection
 
-# Create your views here.
+from friends.models import FriendRequest
+
+
+@login_required()
+def my_friend_requests_view(request):
+    """View to show all the friend requests of a logged-in user"""
+    requests = FriendRequest.objects.filter(
+        receiver=request.user, is_active=True
+    ).only("sender", "receiver")
+    ctx = {"friend_requests": requests}
+    return render(request, "friends/friend_requests.html", ctx)
