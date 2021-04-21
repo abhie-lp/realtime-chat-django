10074c75bfa0cf9d10345edd43c195f824482cac
Abhishek Tiwari <abhie.lp@gmail.com>
2021-04-22 01:24:04 +0530

    Add view to show all friends of a user

--- friends/urls.py
+++ friends/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from .api import send_friend_request_view, accept_friend_request_view, \
     remove_friend_view, decline_friend_request_view, cancel_friend_request
-from .views import my_friend_requests_view
+from .views import my_friend_requests_view, friend_list_view
 
 app_name = "friends"
 
@@ -15,6 +15,7 @@ urlpatterns = [
     path("i-take-back-my-request/<int:pk>/", cancel_friend_request,
          name="cancel_request"),
     path("we-will-not-talk-anymore", remove_friend_view, name="remove_friend"),
+    path("list/<str:username>/", friend_list_view, name="friend_list"),
     path("requests/", my_friend_requests_view, name="friend_requests"),
     path("be-my-friend/", send_friend_request_view, name="send_request"),
 ]
--- friends/views.py
+++ friends/views.py
@@ -3,7 +3,9 @@ from django.http.response import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 from django.db import connection
 
-from friends.models import FriendRequest
+from account.models import Account
+from friends.models import FriendRequest, FriendList
+from utils.search import binary_search
 
 
 @login_required()
@@ -14,3 +16,42 @@ def my_friend_requests_view(request):
     ).only("pk", "sender__username", "sender__profile_image")
     ctx = {"friend_requests": requests}
     return render(request, "friends/friend_requests.html", ctx)
+
+
+@login_required()
+def friend_list_view(request, username):
+    """View to show all users in friend list"""
+    is_self = request.user.username == username
+    account = get_object_or_404(Account, username=username) \
+        if not is_self else request.user
+    friend_list = get_object_or_404(FriendList, user=account)
+
+    # Check if viewing other profile and logged-in user is in their list
+    if not is_self and \
+            request.user.pk not in \
+            friend_list.friends.values_list("pk", flat=True):
+        return HttpResponseForbidden("You are not allowed view friend list")
+
+    if is_self:
+        # If viewing my own friend list
+        # friends = [(account, friendship_status_with_me = True)]
+        friends = [
+            (friend, True)
+            for friend in friend_list.friends.only(
+                "pk", "email", "username", "profile_image"
+            )
+        ]
+    else:
+        # If viewing other accont's friend list
+        my_friends = tuple(FriendList.objects.get(user=request.user).friends
+                           .order_by("pk")
+                           .values_list("pk", flat=True))
+        # friends = [(account, friendship_status_with_me)]
+        friends = [
+            (user_account, binary_search(user_account.pk, my_friends))
+            for user_account in friend_list.friends.only(
+                "pk", "username", "email", "profile_image"
+            )
+        ]
+    ctx = {"friends": friends}
+    return render(request, "friends/friend_list.html", ctx)
