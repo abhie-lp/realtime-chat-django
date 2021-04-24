c582b5051c136ed1334f4dd46a28901d1f0fff7d
Abhishek Tiwari <abhie.lp@gmail.com>
2021-04-24 20:47:32 +0530

    Send chat message to all users

--- personal/views.py
+++ personal/views.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def home_screen_view(request, *args, **kwargs):
     """View for the homescreen"""
     ctx = {
--- public_chat/consumers.py
+++ public_chat/consumers.py
@@ -15,11 +15,45 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
         print("PublicChat connect", self.scope["user"])
         await self.accept()
 
+        # Add them to group
+        await self.channel_layer.group_add(
+            "public_chatroom_1",
+            self.channel_name
+        )
+
     async def disconnect(self, code):
         """Handle the close request from client websocket"""
         print("PublicChatConsumer", "disconnect")
 
     async def receive_json(self, content, **kwargs):
         """Handle the data sent from the client websocket"""
-        command = content.get("command", None)
-        print("PublicChatConsumer", "receive_json", command)
+        command: str = content.get("command", None)
+        message: str = content.get("message", "")
+
+        if command == "send":
+            if not message.strip():
+                raise RuntimeError("You can't send an empty message.")
+            await self.send_message(message)
+
+    async def send_message(self, message):
+        await self.channel_layer.group_send(
+            "public_chatroom_1",
+            {
+                "type": "chat.message",     # chat_message
+                "profile_image": self.scope["user"].profile_image.url,
+                "username":  self.scope["user"].username,
+                "user_id":  self.scope["user"].id,
+                "message":  message,
+            }
+        )
+
+    async def chat_message(self, event):
+        """Handle when there is a message"""
+
+        print("PublicChatConsumer", "chat_message from user", event["user_id"])
+        await self.send_json({
+            "profile_image": event["profile_image"],
+            "username": event["username"],
+            "user_id": event["user_id"],
+            "message": event["message"],
+        })
