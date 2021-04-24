815fc1ab94d9ebd7feb6698a858ff993a448ddb4
Abhishek Tiwari <abhie.lp@gmail.com>
2021-04-24 22:43:27 +0530

    Handle empty message from client

--- public_chat/consumers.py
+++ public_chat/consumers.py
@@ -4,7 +4,10 @@ from json import loads, dumps
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from django.contrib.auth import get_user_model
 
+from utils.exceptions import ClientError
+
 User = get_user_model()
+MSG_TYPE_MESSAGE = 0
 
 
 class PublicChatConsumer(AsyncJsonWebsocketConsumer):
@@ -30,12 +33,19 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
         command: str = content.get("command", None)
         message: str = content.get("message", "")
 
-        if command == "send":
-            if not message.strip():
-                raise RuntimeError("You can't send an empty message.")
-            await self.send_message(message)
+        try:
+            if command == "send":
+                if not message.strip():
+                    raise ClientError(422, "You can't send an empty message.")
+                await self.send_message(message)
+        except ClientError as e:
+            error_data = {"error": e.code}
+            if e.message:
+                error_data["message"] = e.message
+            await self.send_json(error_data)
 
     async def send_message(self, message):
+        """Send message with data"""
         user = self.scope["user"]
         await self.channel_layer.group_send(
             "public_chatroom_1",
@@ -54,6 +64,7 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
 
         print("PublicChatConsumer", "chat_message from user", event["user_id"])
         await self.send_json({
+            "msg_type": MSG_TYPE_MESSAGE,
             "profile_image": event["profile_image"],
             "username": event["username"],
             "user_id": event["user_id"],
