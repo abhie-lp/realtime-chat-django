268894928152cb88dd99d26150c754bea302f44f
Abhishek Tiwari <abhie.lp@gmail.com>
2021-05-12 01:05:23 +0530

    Start chat with user from details, search and friend list page

--- private_chat/views.py
+++ private_chat/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render
 from django.db.models import Q
+from django.http.response import HttpResponseNotFound
 from django.contrib.auth.decorators import login_required
 
 from private_chat.models import PrivateChatRoom
@@ -10,6 +11,20 @@ from private_chat.models import PrivateChatRoom
 def private_chat_room_view(request):
     """View for private chat room"""
     user = request.user
+    meeting_with = request.GET.get("meeting-with")
+
+    # Check if room exists between two users.
+    if meeting_with and not \
+            PrivateChatRoom.objects.filter(
+                Q(user1=user, user2__username=meeting_with) |
+                Q(user1__username=meeting_with, user2=user),
+                is_active=True
+            ).exists():
+        return HttpResponseNotFound(
+            f"You are not friends with {meeting_with}. "
+            f"Add {meeting_with} as friend to start chat."
+        )
+
     # Get the private chat rooms where user1 = user or user2 = user
     rooms = PrivateChatRoom.objects.select_related("user1", "user2").filter(
         Q(user1=user) | Q(user2=user),
@@ -27,4 +42,5 @@ def private_chat_room_view(request):
     ]
     ctx = {"debug_mode": settings.DEBUG,
            "friend_msg": frnd_msg}
+
     return render(request, "private_chat/chat_room.html", ctx)
