41223bf1f7e353e842304fcdc9c81ed0d59c4b8e
Abhishek Tiwari <abhie.lp@gmail.com>
2021-05-14 14:46:23 +0530

    Add Notification model

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    """Admin for Notification model"""
+    list_display = ("id", "for_user", "by_user", "read", "created_at")
+    list_filter = ("read", "created_at", "content_type")
+    search_fields = ("for_user__username", "by_user__username")
--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models import Q
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+
+class Notification(models.Model):
+    """Model for user notification"""
+
+    for_user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 on_delete=models.CASCADE,
+                                 related_name="notification_for")
+    by_user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
+                                on_delete=models.CASCADE, blank=True,
+                                related_name="notification_by")
+    redirect_url = models.URLField(max_length=256, null=True, blank=True)
+    description = models.CharField(max_length=256, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+    def __str__(self):
+        return f"{self.by_user.username} -> {self.for_user.username}"
