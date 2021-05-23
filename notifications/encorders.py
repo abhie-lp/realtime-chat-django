75e43ff400e919b2ae9d69ae92bc2dea1fa76ccc
Abhishek Tiwari <abhie.lp@gmail.com>
2021-05-23 23:56:38 +0530

    Create encoder to serializer notification object

---
+++ notifications/encorders.py
@@ -0,0 +1,26 @@
+"""Serialize notification app models"""
+
+from django.core.serializers.python import Serializer
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
+from .models import Notification
+
+
+class LazyNotificationEncoder(Serializer):
+    """Serializer for notifications"""
+
+    def get_dump_object(self, obj: Notification):
+        """Serializer the notification object"""
+        return {
+            "notification_type": obj.get_model,
+            "id": obj.pk,
+            "description": obj.description,
+            "is_active": (obj.content_type.is_active
+                          if obj.get_model == "friendrequest" else None),
+            "is_read": obj.read,
+            "natural_timestamp": naturaltime(obj.created_at),
+            "timestamp": obj.created_at,
+            "redirect_url": obj.redirect_url,
+            "image_url": (obj.by_user.profile_url.url
+                          if obj.by_user.profile_url else None),
+        }
