e1ed3f0ac1a759579c17fc4505cab90a63eaba56
abhie-lp <abhie.lp@gmail.com>
2025-04-12 00:20:04 +0530

    Convert the home function to async

--- personal/views.py
+++ personal/views.py
@@ -7,4 +7,5 @@ from django.contrib.auth.decorators import login_required
 async def home_screen_view(request, *args, **kwargs):
     """View for the homescreen"""
     ctx = {"debug_mode": settings.DEBUG, "room_id": 1}
+    request.user = await request.auser()
     return render(request, "personal/home.html", ctx)
