4818826d0c800a059ab8892c5aeca207e8b005fd
abhie-lp <abhie.lp@gmail.com>
2025-04-23 13:49:23 +0530

    Accept/Deny request from notification.

--- notifications/utils.py
+++ notifications/utils.py
@@ -1,5 +1,6 @@
 from django.core.serializers.python import Serializer
 from django.contrib.humanize.templatetags.humanize import naturaltime
+from django.urls import reverse
 
 
 class LazyNotificationEncoder(Serializer):
@@ -15,14 +16,22 @@ class LazyNotificationEncoder(Serializer):
             "natural_timestamp": str(naturaltime(obj.created_at)),
             "timestamp": obj.created_at.strftime("%Y-%m-%y %H:%M:%S"),
             "actions": {
-                    "redirect_url": obj.redirect_url,
+                "redirect_url": obj.redirect_url,
             },
             "from": {
                 "image_url": img.url if (img := obj.by_user.profile_image) else None
-            }
+            },
         }
-        if content_type == "friendrequest":
-            dump_object["active"] = obj.content_object.is_active
-            return dump_object
-        elif content_type == "friendlist":
-            return dump_object
+        match content_type:
+            case "friendrequest":
+                dump_object["active"] = obj.content_object.is_active
+                request_id = obj.content_object.pk
+                dump_object["actions"]["accept"] = reverse(
+                    "friends:accept_request", args=(request_id,)
+                )
+                dump_object["actions"]["deny"] = reverse(
+                    "friends:decline_request", args=(request_id,)
+                )
+                return dump_object
+            case "friendlist":
+                return dump_object
